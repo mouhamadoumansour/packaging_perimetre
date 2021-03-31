b652df2f53fb5c70579f55265a0b94a69d5e16c6
mouhamadoumansour <60322068+mouhamadoumansour@users.noreply.github.com>
2021-03-31 17:54:29 +0000

    Add files via upload

---
+++ src/perimetre/__init__.py
@@ -0,0 +1,15 @@
+def triangle(a,b,c):
+    PT = a+b+c
+    return PT
+
+def carre(c):
+    PC = 4*c
+    return PC
+
+def rectangle(long,larg):
+    PR = (long+larg)*2
+    return PR
+
+def cercle(r):
+    PCercle = 2*3.14*r
+    return PCercle
